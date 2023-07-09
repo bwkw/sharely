b4383b84734b2f2a41804ede2401ce4c94a9e988
bwkw <na.shota.1139@gmail.com>
2023-07-09 00:33:34 -0700

    :sparkles: Implement main

--- server/app/main.go
+++ server/app/main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
-	"net/http"
+	"database/sql"
 
+	"github.com/bwkw/sharely/internal/infrastructure/repository/rdb"
+	handler "github.com/bwkw/sharely/internal/interface/http"
+	"github.com/bwkw/sharely/internal/usecase/usecase"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
-	"github.com/bwkw/calendar"
 )
 
-type Server struct{}
-
-func (s *Server) PostGroups(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, "PostGroups called")
-}
-
 func main() {
+	db, err := sql.Open("mysql", "calendar:testtest@/calendar")
+	if err != nil {
+		panic(err)
+	}
+
 	e := echo.New()
-	server := &Server{}
 
-	calendar.RegisterHandlers(e, server)
+	GROUP_BASE_URL := "api/groups"
+	gr := rdb.NewGroupRepository(db)
+	gu := usecase.NewGroupUsecase(gr)
 
+	handler.NewGroupHandler(e, gu, GROUP_BASE_URL)
+
+	e.Logger.Info("Server is running at http://localhost:8080")
 	e.Logger.Fatal(e.Start(":8080"))
 }
