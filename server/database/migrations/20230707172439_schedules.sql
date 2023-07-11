fd90e71c8b86076ace4bd8c40126e32af5d6454f
bwkw <na.shota.1139@gmail.com>
2023-07-11 06:21:28 -0700

    :sparkles: Change directory structure

--- server/database/migrations/20230707172439_schedules.sql
+++ server/database/migrations/20230707172439_schedules.sql
@@ -12,8 +12,8 @@ CREATE TABLE schedules(
   group_id INT NOT NULL,
   created_at DATETIME,
   updated_at DATETIME,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-  FOREIGN KEY (group_id) REFERENCES groups(id)
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (group_id) REFERENCES `groups`(id)
 );
 
 -- +goose Down
