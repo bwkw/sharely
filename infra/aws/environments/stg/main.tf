7790e2db121edf4c43f7928b0d3bba6f7791b5d3
bwkw <na.shota.1139@gmail.com>
2023-08-25 06:07:46 -0700

    :shirt: Format by terraform fmt

--- infra/aws/environments/stg/main.tf
+++ infra/aws/environments/stg/main.tf
@@ -1,19 +1,19 @@
 module "vpc" {
-  environment         = var.environment
-  app_name            = var.app_name
-  
-  source              = "../../modules/vpc"
-  vpc_cidr            = var.vpc_cidr
-  
+  environment = var.environment
+  app_name    = var.app_name
+
+  source   = "../../modules/vpc"
+  vpc_cidr = var.vpc_cidr
+
   availability_zone_a = var.availability_zone_a
   availability_zone_b = var.availability_zone_b
 
-  pub_sub_1a_cidr     = var.pub_sub_1a_cidr
-  pub_sub_1b_cidr     = var.pub_sub_1b_cidr
-  
-  pri1_sub_1a_cidr    = var.pri1_sub_1a_cidr
-  pri2_sub_1a_cidr    = var.pri2_sub_1a_cidr
-  pri1_sub_1b_cidr    = var.pri1_sub_1b_cidr
-  pri2_sub_1b_cidr    = var.pri2_sub_1b_cidr
-  allow_ip_list       = var.allow_ip_list
+  pub_sub_1a_cidr = var.pub_sub_1a_cidr
+  pub_sub_1b_cidr = var.pub_sub_1b_cidr
+
+  pri1_sub_1a_cidr = var.pri1_sub_1a_cidr
+  pri2_sub_1a_cidr = var.pri2_sub_1a_cidr
+  pri1_sub_1b_cidr = var.pri1_sub_1b_cidr
+  pri2_sub_1b_cidr = var.pri2_sub_1b_cidr
+  allow_ip_list    = var.allow_ip_list
 }
