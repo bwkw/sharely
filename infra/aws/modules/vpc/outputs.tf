29a4f034f8b9708ad53d6fc11546ee51dd8548f5
bwkw <na.shota.1139@gmail.com>
2023-08-25 05:06:14 -0700

    :sparkles: Change outputs.tf

--- infra/aws/modules/vpc/outputs.tf
+++ infra/aws/modules/vpc/outputs.tf
@@ -3,7 +3,32 @@ output "vpc_id" {
   description = "ID of the created VPC"
 }
 
-output "subnet_id" {
+output "subnet_pub_1a_id" {
   value       = aws_subnet.pub_1a.id
-  description = "ID of the created subnet"
+  description = "ID of the first created public subnet in AZ 1a"
+}
+
+output "subnet_pub_1b_id" {
+  value       = aws_subnet.pub_1b.id
+  description = "ID of the first created public subnet in AZ 1b"
+}
+
+output "subnet_pri1_1a_id" {
+  value       = aws_subnet.pri1_1a.id
+  description = "ID of the first created private subnet in AZ 1a"
+}
+
+output "subnet_pri2_1a_id" {
+  value       = aws_subnet.pri2_1a.id
+  description = "ID of the second created private subnet in AZ 1a"
+}
+
+output "subnet_pri1_1b_id" {
+  value       = aws_subnet.pri1_1b.id
+  description = "ID of the first created private subnet in AZ 1b"
+}
+
+output "subnet_pri2_1b_id" {
+  value       = aws_subnet.pri2_1b.id
+  description = "ID of the second created private subnet in AZ 1b"
 }
