dfe6683a74b23538ea49837a4ddb4dd003ff1db2
bwkw <na.shota.1139@gmail.com>
2023-08-25 04:57:29 -0700

    :sparkles: Change variables

--- infra/aws/modules/vpc/variables.tf
+++ infra/aws/modules/vpc/variables.tf
@@ -1,10 +1,15 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+variable "environment" {
+  description = "Environment name (e.g., 'stg', 'prod')"
   type        = string
 }
 
-variable "pub_sub_1a_cidr" {
-  description = "CIDR block for the public subnet in AZ 1a"
+variable "app_name" {
+  description = "Application name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
   type        = string
 }
 
@@ -13,13 +18,38 @@ variable "availability_zone_a" {
   type        = string
 }
 
-variable "environment" {
-  description = "Environment name (e.g., 'stg', 'prod')"
+variable "availability_zone_b" {
+  description = "Availability Zone for the 1b subnet"
   type        = string
 }
 
-variable "app_name" {
-  description = "Application name"
+variable "pub_sub_1a_cidr" {
+  description = "CIDR block for the public subnet in AZ 1a"
+  type        = string
+}
+
+variable "pub_sub_1b_cidr" {
+  description = "CIDR block for the public subnet in AZ 1b"
+  type        = string
+}
+
+variable "pri1_sub_1a_cidr" {
+  description = "CIDR block for the first private subnet in AZ 1a"
+  type        = string
+}
+
+variable "pri2_sub_1a_cidr" {
+  description = "CIDR block for the second private subnet in AZ 1a"
+  type        = string
+}
+
+variable "pri1_sub_1b_cidr" {
+  description = "CIDR block for the first private subnet in AZ 1b"
+  type        = string
+}
+
+variable "pri2_sub_1b_cidr" {
+  description = "CIDR block for the second private subnet in AZ 1b"
   type        = string
 }
 
