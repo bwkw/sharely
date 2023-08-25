acd6dfa240851169ced50e68d33f4a4d9db0eaf2
bwkw <na.shota.1139@gmail.com>
2023-08-25 05:23:23 -0700

    :sparkles: Change vpc stg variables

--- infra/aws/environments/stg/variables.tf
+++ infra/aws/environments/stg/variables.tf
@@ -1,21 +1,45 @@
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "pub_sub_1a_cidr" {
-  default = "10.0.1.0/24"
-}
-
-variable "availability_zone_a" {
-  default = "ap-northeast-1a"
-}
-
 variable "environment" {
   default = "stg"
 }
 
 variable "app_name" {
   default = "sharely"
+}
+
+variable "vpc_cidr" {
+  default = "192.168.0.0/24"
+}
+
+variable "availability_zone_a" {
+  default = "ap-northeast-1a"
+}
+
+variable "availability_zone_b" {
+  default = "ap-northeast-1b"
+}
+
+variable "pub_sub_1a_cidr" {
+  default = "192.168.0.0/28"
+}
+
+variable "pub_sub_1b_cidr" {
+  default = "192.168.0.16/28"
+}
+
+variable "pri1_sub_1a_cidr" {
+  default = "192.168.0.32/28"
+}
+
+variable "pri2_sub_1a_cidr" {
+  default = "192.168.0.48/28"
+}
+
+variable "pri1_sub_1b_cidr" {
+  default = "192.168.0.64/28"
+}
+
+variable "pri2_sub_1b_cidr" {
+  default = "192.168.0.80/28"
 }
 
 variable "allow_ip_list" {
