1a2367465e84f2a000f5a4f3761f0195554ad5ba
bwkw <na.shota.1139@gmail.com>
2023-08-30 01:58:49 -0700

    :sparkles: Add description to variables

--- infra/aws/environments/stg/variables.tf
+++ infra/aws/environments/stg/variables.tf
@@ -1,45 +1,67 @@
 variable "environment" {
-  default = "stg"
+  description = "Environment name (e.g., 'stg', 'prod')"
+  type        = string
+  default     = "stg"
 }
 
 variable "app_name" {
-  default = "sharely"
+  description = "Application name"
+  type        = string
+  default     = "sharely"
 }
 
 variable "vpc_cidr" {
-  default = "192.168.0.0/24"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "192.168.0.0/24"
 }
 
 variable "availability_zone_a" {
-  default = "ap-northeast-1a"
+  description = "Availability Zone for the 1a subnet"
+  type        = string
+  default     = "ap-northeast-1a"
 }
 
 variable "availability_zone_c" {
-  default = "ap-northeast-1c"
+  description = "Availability Zone for the 1c subnet"
+  type        = string
+  default     = "ap-northeast-1c"
 }
 
 variable "pub_sub_1a_cidr" {
-  default = "192.168.0.0/28"
+  description = "CIDR block for the public subnet in AZ 1a"
+  type        = string
+  default     = "192.168.0.0/28"
 }
 
 variable "pub_sub_1c_cidr" {
-  default = "192.168.0.16/28"
+  description = "CIDR block for the public subnet in AZ 1a"
+  type        = string
+  default     = "192.168.0.16/28"
 }
 
 variable "pri1_sub_1a_cidr" {
-  default = "192.168.0.32/28"
+  description = "CIDR block for the first private subnet in AZ 1a"
+  type        = string
+  default     = "192.168.0.32/28"
 }
 
 variable "pri2_sub_1a_cidr" {
-  default = "192.168.0.48/28"
+  description = "CIDR block for the second private subnet in AZ 1a"
+  type        = string
+  default     = "192.168.0.48/28"
 }
 
 variable "pri1_sub_1c_cidr" {
-  default = "192.168.0.64/28"
+  description = "CIDR block for the first private subnet in AZ 1c"
+  type        = string
+  default     = "192.168.0.64/28"
 }
 
 variable "pri2_sub_1c_cidr" {
-  default = "192.168.0.80/28"
+  description = "CIDR block for the second private subnet in AZ 1c"
+  type        = string
+  default     = "192.168.0.80/28"
 }
 
 variable "allow_ip_list" {
