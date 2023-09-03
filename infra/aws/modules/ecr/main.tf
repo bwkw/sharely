a49d7e1cc4c46cf49f8b0696ab15fa3e1fa460d0
bwkw <na.shota.1139@gmail.com>
2023-09-03 05:34:00 -0700

    :+1: Change iteration process for each

--- infra/aws/modules/ecr/main.tf
+++ infra/aws/modules/ecr/main.tf
@@ -1,50 +1,37 @@
-resource "aws_ecr_repository" "next_js" {
-  name = "next-js-front-app"
+locals {
+  apps = {
+    next_js = {
+      name: "next-js-front-app",
+      description: "Keep only 10 images for Next.js frontend app"
+    },
+    go = {
+      name: "go-backend-app",
+      description: "Keep only 10 images for Go backend app"
+    }
+  }
+}
+
+resource "aws_ecr_repository" "app" {
+  for_each = local.apps
+
+  name = each.value.name
 
   tags = {
-    Name        = "next-js-front-app"
+    Name        = each.value.name
     Environment = var.environment
   }
 }
 
-resource "aws_ecr_repository" "go" {
-  name = "go-backend-app"
+resource "aws_ecr_lifecycle_policy" "app" {
+  for_each = local.apps
 
-  tags = {
-    Name        = "go-backend-app"
-    Environment = var.environment
-  }
-}
-
-resource "aws_ecr_lifecycle_policy" "next_js" {
-  repository = aws_ecr_repository.next_js.name
+  repository = aws_ecr_repository.app[each.key].name
 
   policy = jsonencode({
     rules = [
       {
         rulePriority = 1
-        description  = "Keep only 10 images"
-        selection = {
-          tagStatus   = "untagged"
-          countType  = "imageCountMoreThan"
-          countNumber = 10
-        }
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_ecr_lifecycle_policy" "go" {
-  repository = aws_ecr_repository.go.name
-
-  policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1
-        description  = "Keep only 10 images"
+        description  = each.value.description
         selection = {
           tagStatus   = "untagged"
           countType  = "imageCountMoreThan"
