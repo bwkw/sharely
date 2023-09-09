dfb3648485dfacd324fc1a4c1b6c273ede3e06c1
bwkw <na.shota.1139@gmail.com>
2023-09-09 16:34:14 -0700

    :bug: Set alb http

--- infra/aws/modules/alb/main.tf
+++ infra/aws/modules/alb/main.tf
@@ -24,16 +24,16 @@ resource "aws_lb_listener" "http_listener" {
 }
 
 resource "aws_lb_target_group" "pub_alb_tg" {
-  name     = "${var.environment}-${var.app_name}-tg"
+  name     = "${var.app_name}-${var.environment}-pub-alb-tg"
   port     = 80
-  protocol = "HTTPS"
+  protocol = "HTTP"
   vpc_id   = var.vpc_id
 
   health_check {
     enabled             = true
     interval            = 30
     path                = "/"
-    protocol            = "HTTPS"
+    protocol            = "HTTP"
     timeout             = 5
     healthy_threshold   = 3
     unhealthy_threshold = 3
