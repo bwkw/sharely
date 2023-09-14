f2880206b3638503956ad599f3541341718b719a
bwkw <na.shota.1139@gmail.com>
2023-09-14 07:22:30 -0700

    :fire: Remove default settings

--- infra/aws/modules/vpc/main.tf
+++ infra/aws/modules/vpc/main.tf
@@ -3,8 +3,6 @@
 # ---------------------------
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
 
   tags = {
     Name = "${var.app_name}-${var.environment}-vpc"
