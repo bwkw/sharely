33d6a79229639f53440f712f4f5fec7e546eb3f8
bwkw <na.shota.1139@gmail.com>
2023-09-14 06:44:55 -0700

    :sparkles: Add default tag

--- infra/aws/provider.tf
+++ infra/aws/provider.tf
@@ -2,4 +2,10 @@ provider "aws" {
   profile     = "sharely"
   region      = "ap-northeast-1"
   max_retries = 20
+  default_tags {
+    tags = {
+      Environment     = "stg"
+      Service         = "sharely"
+    }
+  }
 }
