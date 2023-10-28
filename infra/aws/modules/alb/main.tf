25919f68ceef48893aaf565cdfd33301cb58e18b
bwkw <na.shota.1139@gmail.com>
2023-10-27 18:14:00 -0700

    :recycle: Change main and output

--- infra/aws/modules/alb/main.tf
+++ infra/aws/modules/alb/main.tf
@@ -11,78 +11,54 @@ terraform {
 
 locals {
   common_name_prefix = "${var.app_name}-${var.environment}"
+
+  alb_configs = {
+    pub = {
+      internal           = false,
+      security_groups    = var.alb_security_group_ids.pub,
+      subnets            = var.alb_subnet_ids.pub
+    },
+    pri1 = {
+      internal           = true,
+      security_groups    = var.alb_security_group_ids.pri1,
+      subnets            = var.alb_subnet_ids.pri1
+    }
+  }
 }
 
-resource "aws_lb" "pub_alb" {
-  name               = "${local.common_name_prefix}-pub-alb"
-  internal           = false
+resource "aws_lb" "alb" {
+  for_each = local.alb_configs
+
+  name               = "${local.common_name_prefix}-${each.key}-alb"
+  internal           = each.value.internal
   load_balancer_type = "application"
-  security_groups    = var.alb_security_group_ids.pub
-  subnets            = var.alb_subnet_ids.pub
+  security_groups    = each.value.security_groups
+  subnets            = each.value.subnets
 
   enable_deletion_protection = false
 
   tags = {
-    Name = "${local.common_name_prefix}-pub-alb"
+    Name = "${local.common_name_prefix}-${each.key}-alb"
   }
 }
 
-resource "aws_lb_listener" "pub_http" {
-  load_balancer_arn = aws_lb.pub_alb.arn
+resource "aws_lb_listener" "http" {
+  for_each = local.alb_configs
+
+  load_balancer_arn = aws_lb.alb[each.key].arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.pub_alb.arn
+    target_group_arn = aws_lb_target_group.alb[each.key].arn
   }
 }
 
-resource "aws_lb_target_group" "pub_alb" {
-  name     = "${local.common_name_prefix}-pub-alb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-  target_type = "ip"
+resource "aws_lb_target_group" "alb" {
+  for_each = local.alb_configs
 
-  health_check {
-    enabled             = true
-    interval            = 30
-    path                = "/"
-    protocol            = "HTTP"
-    timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-  }
-}
-
-resource "aws_lb" "pri_alb" {
-  name               = "${local.common_name_prefix}-pri-alb"
-  internal           = true
-  load_balancer_type = "application"
-  security_groups    = var.alb_security_group_ids.pri1
-  subnets            = var.alb_subnet_ids.pri1
-
-  enable_deletion_protection = false
-
-  tags = {
-    Name = "${local.common_name_prefix}-pri-alb"
-  }
-}
-
-resource "aws_lb_listener" "pri_http" {
-  load_balancer_arn = aws_lb.pri_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.pri_alb.arn
-  }
-}
-
-resource "aws_lb_target_group" "pri_alb" {
-  name     = "${local.common_name_prefix}-pri-alb-tg"
+  name     = "${local.common_name_prefix}-${each.key}-alb-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
