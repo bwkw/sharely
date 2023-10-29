a7da02229ebb042af778587c0f035f331054ab70
bwkw <na.shota.1139@gmail.com>
2023-10-29 05:07:32 -0700

    :recycle: Change ecs output name

--- infra/aws/modules/ecs/outputs.tf
+++ infra/aws/modules/ecs/outputs.tf
@@ -1,19 +1,17 @@
-output "ecs_cluster_arn" {
+output "cluster_arn" {
   value       = aws_ecs_cluster.main.arn
   description = "The ARN of the ECS cluster."
 }
 
-output "next_js_task_definition_arn" {
-  value       = aws_ecs_task_definition.task["next_js"].arn
-  description = "The ARN of the next_js ECS task definition"
+output "task_definition_arn" {
+  description = "The ARN of the  ECS task definition"
+  value       = {
+    frontend     = aws_ecs_task_definition.common["frontend"].arn
+    backend      = aws_ecs_task_definition.common["backend"].arn
+  }
 }
 
-output "go_task_definition_arn" {
-  value       = aws_ecs_task_definition.task["go"].arn
-  description = "The ARN of the go ECS task definition"
-}
-
-output "ecs_execution_role_arn" {
+output "execution_role_arn" {
   description = "The ARN of the ECS execution role"
   value       = aws_iam_role.ecs_execution_role.arn
 }
