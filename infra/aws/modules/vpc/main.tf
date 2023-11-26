a5635e004de15d1560141f0863e7955fd4f70824
bwkw <na.shota.1139@gmail.com>
2023-11-25 22:26:43 -0800

    :bug: Change ecs task outbound rule allow from anywhere

--- infra/aws/modules/vpc/main.tf
+++ infra/aws/modules/vpc/main.tf
@@ -186,18 +186,12 @@ resource "aws_security_group_rule" "frontend_ecs_tasks_ingress" {
 }
 
 resource "aws_security_group_rule" "frontend_ecs_tasks_egress" {
-  for_each = {
-    to_ecr_api_endpoint = { port = 443, sg = aws_security_group.vpc_endpoint_ecr_api.id },
-    to_ecr_dkr_endpoint = { port = 443, sg = aws_security_group.vpc_endpoint_ecr_dkr.id },
-    to_pri_alb          = { port = 80, sg = aws_security_group.pri_alb.id },
-  }
-
-  security_group_id        = aws_security_group.frontend_ecs_tasks.id
-  type                     = "egress"
-  from_port                = each.value["port"]
-  to_port                  = each.value["port"]
-  protocol                 = "tcp"
-  source_security_group_id = each.value["sg"]
+  security_group_id = aws_security_group.frontend_ecs_tasks.id
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 # プライベートALBのセキュリティグループ
@@ -262,19 +256,12 @@ resource "aws_security_group_rule" "backend_ecs_tasks_ingress" {
 }
 
 resource "aws_security_group_rule" "backend_ecs_tasks_egress" {
-  for_each = {
-    to_ecr_api_endpoint         = { port = 443, sg = aws_security_group.vpc_endpoint_ecr_api.id },
-    to_ecr_dkr_endpoint         = { port = 443, sg = aws_security_group.vpc_endpoint_ecr_dkr.id },
-    to_secrets_manager_endpoint = { port = 443, sg = aws_security_group.vpc_endpoint_secrets_manager.id },
-    to_aurora                   = { port = 3306, sg = aws_security_group.aurora.id }
-  }
-
-  security_group_id        = aws_security_group.backend_ecs_tasks.id
-  type                     = "egress"
-  from_port                = each.value["port"]
-  to_port                  = each.value["port"]
-  protocol                 = "tcp"
-  source_security_group_id = each.value["sg"]
+  security_group_id = aws_security_group.backend_ecs_tasks.id
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 # Auroraのセキュリティグループ
